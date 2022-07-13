b6292dc1e771bbb6fa913552012824569b6afcf9
sandilsranasinghe <sandilsranasinghe@gmail.com>
2022-07-13 12:47:20 +0530

    Counter_2.chd, undo file structure changes

---
+++ Lab9.srcs/sources_1/new/Counter_2.vhd
@@ -0,0 +1,46 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07/13/2022 11:38:54 AM
+-- Design Name: 
+-- Module Name: Counter_2 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Counter_2 is
+    Port ( I_Clk : in STD_LOGIC;
+           I_Reset : in STD_LOGIC;
+           O_Q0 : out STD_LOGIC;
+           O_Q1 : out STD_LOGIC);
+end Counter_2;
+
+architecture Behavioral of Counter_2 is
+
+begin
+
+
+end Behavioral;
