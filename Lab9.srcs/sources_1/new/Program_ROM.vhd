8ff6966fbac3de5be38590dbabcc940c0b2b3827
NimeshRancha <nimesh.ranchagoda@gmail.com>
2022-07-17 21:06:32 +0530

    Added Program ROM

--- Lab9.srcs/sources_1/new/Program_ROM.vhd
+++ Lab9.srcs/sources_1/new/Program_ROM.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -37,8 +37,22 @@ entity Program_ROM is
 end Program_ROM;
 
 architecture Behavioral of Program_ROM is
+--start
+type rom_type is array (0 to 7) of std_logic_vector(11 downto 0);
+ 
+ signal sevenSegment_ROM : rom_type := (
+ "100000000000", -- 0    --to be changed
+ "111100100000", --1
+ "010010000000", --2
+ "011000000000", --3
+ "001100100000", --4
+ "001001000000", --5
+ "000001000000", --6
+ "111100000000" --7
+ );
 
 begin
 
+Instruction <= sevenSegment_ROM(to_integer(unsigned(Mem_Sel)));
 
 end Behavioral;
